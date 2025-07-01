ed71b4ef0c0922458d7b0aadd9e06317e7640c46
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-07-01 05:37:28 +0300

    Integration api of search in shop

--- src/app/shop/main-shop/main-shop.component.ts
+++ src/app/shop/main-shop/main-shop.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BookService } from '../../services/book.service'; 
+import { BookService } from '../../services/book.service';
 import { FilterComponent } from '../filter/filter.component';
 import { BookCardComponent } from '../../shared/book-card/book-card.component';
 import { Book } from '../../interfaces/book';
+import { FormsModule } from '@angular/forms';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-main-shop',
   standalone: true,
-  imports: [CommonModule, FilterComponent, BookCardComponent],
+  imports: [CommonModule, FilterComponent, BookCardComponent, FormsModule],
   templateUrl: './main-shop.component.html',
   styleUrls: ['./main-shop.component.css']
 })
@@ -19,14 +21,42 @@ export class MainShopComponent {
   currentFilters: any = {};
   showSearch: boolean = false;
 
-  constructor(private bookService: BookService) {}
+  searchQuery: string = '';
+  private searchSubject = new Subject<string>();
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
     this.loadBooks();
+
+    // اعمل سبسكرايب عشان نعمل debounce على البحث
+    this.searchSubject.pipe(
+      debounceTime(500)
+    ).subscribe(query => {
+      this.performSearch(query);
+    });
   }
 
   toggleSearch() {
     this.showSearch = !this.showSearch;
+    setTimeout(() => {
+      const input = document.getElementById('searchInput') as HTMLInputElement;
+      if (input) input.focus();
+    }, 0);
+  }
+
+  onSearchChange() {
+    this.searchSubject.next(this.searchQuery);
+  }
+
+  performSearch(query: string) {
+    if (!query.trim()) {
+      this.loadBooks();
+      return;
+    }
+    this.bookService.searchBooks(query, this.currentPage, this.itemsPerPage).subscribe(res => {
+      this.books = res.data.books;
+    });
   }
 
   onFiltersApplied(filters: any) {
