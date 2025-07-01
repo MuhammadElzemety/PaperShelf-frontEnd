183c310ff44bf8ff25398d1649daf4c1cc4b881c
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-07-01 06:22:42 +0300

    Edit pagination in Shop Using Api

--- src/app/interfaces/book.ts
+++ src/app/interfaces/book.ts
@@ -1,23 +1,30 @@
 export interface Book {
-    id: string;               // بدل _id
-    title: string;
-    author: string;
-    description: string;
-    isbn: string;
-    price: number;
-    discount: number;
-    pages: number;
-    category: string;
-    coverImage?: string;      // سواء coverImage أو coverImageUrl
-    images: string[];
-    stock: number;
-    rating: number;           // بدل averageRating
-    totalReviews: number;
-    totalSales: number;
-    isNew: boolean;
-    isBestseller: boolean;
-    isFeatured: boolean;
-    createdAt: string;
-    updatedAt: string;
-  }
-  
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  isbn: string;
+  price: number;
+  discount: number;
+  pages: number;
+  category: string;
+  coverImage?: string;
+  images: string[];
+  stock: number;
+  rating: number;
+  totalReviews: number;
+  totalSales: number;
+  isNew: boolean;
+  isBestseller: boolean;
+  isFeatured: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface BookPagination {
+  currentPage: number;
+  totalPages: number;
+  totalBooks: number;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  limit: number;
+}
