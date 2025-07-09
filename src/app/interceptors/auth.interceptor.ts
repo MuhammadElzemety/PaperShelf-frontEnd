9185a811bbf0feca43ed1c4772ff6babaeef3dc6
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-07-09 19:34:51 +0300

    fix token expiration: refresh or logout

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,10 +2,16 @@ import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { catchError, switchMap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('accessToken');
   const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (req.url.includes('/auth/refresh-token')) {
+    return next(req);
+  }
 
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
@@ -13,18 +19,32 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(authReq).pipe(
     catchError((error: HttpErrorResponse) => {
-      // if (error.status === 401) {
-      //   console.log('[authInterceptor] Got 401, trying to refresh token...');
-      //   return authService.refreshAccessToken().pipe(
-      //     switchMap(() => {
-      //       const newToken = localStorage.getItem('accessToken');
-      //       const newAuthReq = req.clone({
-      //         setHeaders: { Authorization: `Bearer ${newToken}` }
-      //       });
-      //       return next(newAuthReq);
-      //     })
-      //   );
-      // }
+      if (error.status === 401) {
+        return authService.refreshAccessToken().pipe(
+          switchMap(() => {
+            const newToken = localStorage.getItem('accessToken');
+            if (!newToken) {
+              
+              authService.logout();
+              router.navigate(['/login']);
+              return throwError(() => error);
+            }
+
+            const newAuthReq = req.clone({
+              setHeaders: { Authorization: `Bearer ${newToken}` }
+            });
+            
+            return next(newAuthReq);
+          }),
+          catchError((refreshError) => {
+            
+            authService.logout();
+            router.navigate(['/login']);
+            return throwError(() => refreshError);
+          })
+        );
+      }
+
       return throwError(() => error);
     })
   );
