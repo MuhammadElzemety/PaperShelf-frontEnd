c2f19a03357bdc1f958b73ae3960fcc734a4f4de
Muhammad <Muhammadabdelwahhab2@gmail.com>
2025-07-12 07:38:00 +0300

    PrdouctionUpdate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: true,
-    apiBase: 'https://your-production-domain.com/',
-    apiBaseUrl: 'https://your-production-domain.com/',
-    apiUrlForImgs: 'https://your-production-domain.com/'
+  production: true,
+  apiBase: 'https://papershelf-backend-production.up.railway.app/api/',
+  apiBaseUrl: 'https://papershelf-backend-production.up.railway.app/api/v1/',
+  apiUrlForImgs: 'https://papershelf-backend-production.up.railway.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
-    production: false,
-    apiBase: 'http://localhost:3000/api',
-    apiBaseUrl: 'http://localhost:3000/api/v1',
-    apiUrlForImgs:'http://localhost:3000/',
+  production: false,
+  apiBase: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://localhost:3000/api/v1',
+  apiUrlForImgs: 'http://localhost:3000/',
 };
-
 
 // const API_URL = `${environment.apiBaseUrl}/books`;
 // const IMG_URL = `${environment.apiUrlForImgs}`;
